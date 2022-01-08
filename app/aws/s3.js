6be587c70c403db9e4c82e43974d9c310dbd0aa5
wsierakowski <wsierakowski@gmail.com>
2022-01-08 11:20:16 +0000

    update to read region from env var

--- app/aws/s3.js
+++ app/aws/s3.js
@@ -1,4 +1,6 @@
 import { S3 } from "@aws-sdk/client-s3";
-const REGION = "eu-west-2";
+import config from '../utils/config.js';
+
+const REGION = config.get('AWS_REGION') || "eu-west-2";
 const s3 = new S3({region: REGION});
 export { s3 };
